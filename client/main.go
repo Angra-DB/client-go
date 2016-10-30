062ecd110dd6906a899711325f959979d1c0aa15
Ismael Medeiros <140083162@aluno.unb.br>
2016-10-29 22:24:08 -0200

    Fixed JSON verification

--- client/main.go
+++ client/main.go
@@ -2,44 +2,34 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"os"
-  "encoding/json"
-  "strings"
+	"strings"
 )
 
 func isJSONString(s string) bool {
-    var js string
-    return json.Unmarshal([]byte(s), &js) == nil
+	var js string
+	return json.Unmarshal([]byte(s), &js) == nil
 
 }
 
 func isJSON(s string) bool {
-    var js map[string]interface{}
-    return json.Unmarshal([]byte(s), &js) == nil
+	var js map[string]interface{}
+	return json.Unmarshal([]byte(s), &js) == nil
 
 }
-//TODO: remove comentary above
-// bufferIn := bufio.NewReader(os.Stdin)
-// messageIn, InErr := bufferIn.ReadString('\n')
-// if InErr != nil {
-//   log.Fatal("\nAn error occured: ", InErr)
-// }
-// fmt.Printf("isJSONString(%s) = %v\n", messageIn, isJSONString(messageIn))
-// fmt.Printf("isJSON(%s) = %v\n\n", messageIn, isJSON(messageIn))
-
-
 
 /**
 * This is the main function. It recieves two arguments,
-* the first one is the IP.
+* the first one is the 	 IP.
 * The second one is the Port.
  */
 func main() {
-  connIp := os.Args[1]
-  port := os.Args[2]
+	connIp := os.Args[1]
+	port := os.Args[2]
 	conn := conn_server(connIp, port)
 	commandProcess(conn)
 }
@@ -48,11 +38,11 @@ func main() {
 * This function connects to the server based, in TCP protocol.
 * It tries to connect. If some error occur, returns the error.
 * Otherwise, return the connection.
-*/
+ */
 func conn_server(host string, port string) net.Conn {
 	fmt.Print("\nTrying to connect to " + host + "....")
 
-	conn, connErr := net.Dial("tcp", host + ":" + port)
+	conn, connErr := net.Dial("tcp", host+":"+port)
 	if connErr != nil {
 		log.Fatal("\nAn error occured: ", connErr)
 	}
@@ -75,36 +65,55 @@ func commandProcess(conn net.Conn) {
 		if InErr != nil {
 			log.Fatal("\nAn error occured: ", InErr)
 		}
-    messageIn = messageIn[:len(messageIn)-1]
+		messageIn = messageIn[:len(messageIn)-1]
 
-		fmt.Fprint(conn, messageIn)
-
-    var json_buffer string
-    if strings.HasPrefix(messageIn, "save"){
-      json_buffer = messageIn[len("save"):len(messageIn)]
-    } else if strings.HasPrefix(messageIn, "lookup"){
-      json_buffer = messageIn[len("lookup"):len(messageIn)]
-    } else if strings.HasPrefix(messageIn, "delete"){
-      json_buffer = messageIn[len("delete"):len(messageIn)]
-    } else if strings.HasPrefix(messageIn, "update"){
-      json_buffer = messageIn[len("update"):len(messageIn)]
-    }
-    
-    if(isJSON(json_buffer) == true){
-      fmt.Println("É JSON.")
-    } else{
-      fmt.Println("Não é JSON.")
-    }
-
-    fmt.Println(json_buffer)
-
-		bufferOut := bufio.NewReader(conn)
-    fmt.Println("Entrando no reader da saida:")
-		_, _,OutErr := bufferOut.ReadLine()
-    fmt.Println("Saindo do reader da saida")
-		if OutErr != nil {
-			log.Fatal("\nAn error occured: ", OutErr)
+		if messageIn == "quit" {
+			conn.Close()
+			os.Exit(0)
 		}
-		// fmt.Println(messageOut)
+
+		var json_buffer string
+		if strings.HasPrefix(messageIn, "save") {
+
+			json_buffer = messageIn[len("save "):len(messageIn)]
+
+		} else if strings.HasPrefix(messageIn, "lookup") {
+
+			json_bufferB, _ := json.Marshal(messageIn[len("lookup "):len(messageIn)])
+			json_buffer = string(json_bufferB)
+
+		} else if strings.HasPrefix(messageIn, "delete") {
+
+			json_bufferB, _ := json.Marshal(messageIn[len("delete "):len(messageIn)])
+			json_buffer = string(json_bufferB)
+
+		} else if strings.HasPrefix(messageIn, "update") {
+			
+			json_buffer = messageIn[len("update "):len(messageIn)]
+
+		} else if strings.HasPrefix(messageIn, "create_db") {
+
+			json_buffer = messageIn[len("create_db "):len(messageIn)]
+
+		} else if strings.HasPrefix(messageIn, "connect") {
+
+			json_buffer = messageIn[len("connect "):len(messageIn)]
+
+		}
+
+		if isJSON(json_buffer) || isJSONString(json_buffer) {
+			fmt.Println("É JSON.")
+
+			fmt.Fprint(conn, messageIn)
+
+			bufferOut := bufio.NewReader(conn)
+			messageOut, OutErr := bufferOut.ReadString('\n')
+			if OutErr != nil {
+				log.Fatal("\nAn error occured: ", OutErr)
+			}
+			fmt.Println(messageOut)
+		} else {
+			fmt.Println("Não é JSON.")
+		}
 	}
 }
